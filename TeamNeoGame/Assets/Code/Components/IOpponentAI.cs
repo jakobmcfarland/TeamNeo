4eadb902f2b261bdc3f15204fc9bc928e0792522
Cytosolic <BigTigerGun@outlook.com>
2019-04-26 09:57:50 -0700

    Started ground work on the prototype.

---
+++ TeamNeoGame/Assets/Code/Components/IOpponentAI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Code.Components
+{
+    interface IOpponentAI
+    {
+
+
+        void StartAttack( );
+
+        void FinishAttack( );
+
+        int GetAttackTime( );
+
+        void SetAttackTime(int time);
+    }
+}
