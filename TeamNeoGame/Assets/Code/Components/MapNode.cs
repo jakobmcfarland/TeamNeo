a4313b4e37fe703d3d05e917c6ed3ed1af8e5b8b
Jakob McFarland <jakob.mcfarland@digipen.edu>
2019-04-26 16:28:55 -0700

    Did a bit more ground work for the map.

---
+++ TeamNeoGame/Assets/Code/Components/MapNode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.CoreModule;
+using UnityEngine;
+
+public class MapNode : MonoBehaviour
+{
+    Vector normalColor = 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update when down
+    void OnMouseDown()
+    {
+
+
+    }
+
+    // Update when clicked
+    void OnMouseUpAsButton()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+
+
+    }
+
+    void OnMouseExit()
+    {
+
+
+    }
+}
