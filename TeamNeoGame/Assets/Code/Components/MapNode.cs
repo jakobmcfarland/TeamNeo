da6825031f174ac220f88e2531615bd2f514be89
Cytosolic <BigTigerGun@outlook.com>
2019-04-30 00:53:11 -0700

    Made a base node system for the map progression system.

--- TeamNeoGame/Assets/Code/Components/MapNode.cs
+++ TeamNeoGame/Assets/Code/Components/MapNode.cs
@@ -2,38 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum NodeState
+{
+    Incomplete,
+    Complete,
+    Current
+}
+
 public class MapNode : MonoBehaviour
 {
-   // Vector normalColor = 
+    public SpriteRenderer spriteRenderer;
+
+    public Color incompleteNormalColor = Color.white;
+    public Color completeNormalColor = new Color(0.35f, 0.35f, 0.35f);
+    public Color currentNormalColor = Color.blue;
+    public Color overColor = Color.grey;
+    public Color downColor = new Color(0.35f, 0.35f, 0.35f);
+
+    public MapNode previusObjectA;
+    public MapNode previusObjectB;
+
+    public MapNode previusNodeA;
+    public MapNode previusNodeB;
+
+    public NodeState nodeState = NodeState.Incomplete;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        previusNodeA = previusObjectA.GetComponent<MapNode>();
+        previusNodeB = previusObjectB.GetComponent<MapNode>();
+
+        if (nodeState == NodeState.Current) spriteRenderer.color = currentNormalColor;
+    }
+
+    void OnMouseEnter()
+    {
+        if(previusNodeA.nodeState == NodeState.Current) spriteRenderer.color = overColor;
+    }
+
+    void OnMouseExit()
+    {
+        if (previusNodeA.nodeState == NodeState.Current) spriteRenderer.color = incompleteNormalColor;
     }
 
     // Update when down
     void OnMouseDown()
     {
-
-
+        if (previusNodeA.nodeState == NodeState.Current) spriteRenderer.color = downColor;
     }
 
     // Update when clicked
     void OnMouseUpAsButton()
     {
-
+        if (nodeState == NodeState.Incomplete &&
+            ( previusNodeA.nodeState == NodeState.Current
+            || previusNodeB.nodeState == NodeState.Current))
+        {
+            ActivateNode();
+        }
     }
 
-    void OnMouseEnter()
+    void ActivateNode()
     {
+        spriteRenderer.color = currentNormalColor;
+        nodeState = NodeState.Current;
 
-
-    }
-
-    void OnMouseExit()
-    {
-
-
+        previusNodeA.spriteRenderer.color = completeNormalColor;
+        previusNodeB.spriteRenderer.color = completeNormalColor;
+        previusNodeA.nodeState = NodeState.Complete;
+        previusNodeB.nodeState = NodeState.Complete;
     }
 }
