31104bdc67e991431b35961d92c3b282be0a369b
Cytosolic <BigTigerGun@outlook.com>
2019-05-04 16:40:25 -0700

    Created MapNodeLink prefab with animation, started working on MapPlayer animations.

--- TeamNeoGame/Assets/Code/Components/MapPlayerController.cs
+++ TeamNeoGame/Assets/Code/Components/MapPlayerController.cs
@@ -7,15 +7,26 @@ public class MapPlayerController : MonoBehaviour
     public float mapPlayerWalkSpeed = 15.0f;
     public float mapPlayerMaxSpeed = 15.0f;
 
+    Rigidbody2D rigidbody;
+    Animator animator;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector2 movement = new Vector2(0.0f, 0.0f);
 
         movement.x = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("playerHorizontalSpeed", movement.x);
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("playerVerticalSpeed", movement.y);
         movement = movement.normalized * mapPlayerWalkSpeed;
 
-        GetComponent<Rigidbody2D>().velocity = movement;
+        rigidbody.velocity = movement;
     }
 }
---
+++ TeamNeoGame/Assets/Code/HPBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    public float maxSize = 3.743f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.transform.localScale = new Vector3(maxSize, 0.5f, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateHP(float percent)
+    {
+        sprite.transform.localScale = new Vector3(percent * maxSize, 0.5f, 1);
+    }
+}
