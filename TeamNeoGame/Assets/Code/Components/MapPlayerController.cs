e35ee79d722110856a1bbc1a49f2fd648b9f7afa
Cytosolic <BigTigerGun@outlook.com>
2019-05-05 17:26:30 -0700

    Finished MapPlayer animation.

--- TeamNeoGame/Assets/Code/Components/MapPlayerController.cs
+++ TeamNeoGame/Assets/Code/Components/MapPlayerController.cs
@@ -8,11 +8,13 @@ public class MapPlayerController : MonoBehaviour
     public float mapPlayerMaxSpeed = 15.0f;
 
     Rigidbody2D rigidbody_;
+    SpriteRenderer spriteRenderer_;
     Animator animator;
 
     private void Start()
     {
         rigidbody_ = GetComponent<Rigidbody2D>();
+        spriteRenderer_ = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
 
@@ -22,10 +24,16 @@ public class MapPlayerController : MonoBehaviour
         Vector2 movement = new Vector2(0.0f, 0.0f);
 
         movement.x = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("playerHorizontalSpeed", movement.x);
+        animator.SetFloat("playerHorizontalSpeed", Mathf.Abs(movement.x));
         movement.y = Input.GetAxisRaw("Vertical");
         animator.SetFloat("playerVerticalSpeed", movement.y);
         movement = movement.normalized * mapPlayerWalkSpeed;
+
+        if (movement.y == 0.0f)
+        {
+            if (movement.x > 0) spriteRenderer_.flipX = true;
+            else spriteRenderer_.flipX = false;
+        }
 
         rigidbody_.velocity = movement;
     }
