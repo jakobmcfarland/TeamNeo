d03d785112c162f85b5ff55113e9d7ea1811cb98
nicohman <nicohman@demenses.net>
2019-05-12 18:09:38 -0700

    Add in mockup map sprite

--- TeamNeoGame/Assets/Code/Components/TextBox.cs
+++ TeamNeoGame/Assets/Code/Components/TextBox.cs
@@ -22,44 +22,5 @@ public class TextBox
     }
 }
 public class TBox: MonoBehaviour {
-	public string [] dialogue;
-	public float timePerBox = 2.0f;
-	private int index = 0;
-	public KeyCode nextKey;
-	private TextMeshProUGUI textBox;
-	private SpriteRenderer textSprite;
-    private bool done;
-    private float timer;
-	void Start() {
-		textBox = GetComponent<TextMeshProUGUI>();
-		textSprite.enabled = enabled;
-	}
-	void Update() {
-        if (Input.GetKeyDown(nextKey))
-        {
-            if (!done)
-            {
-                done = true;
-                textBox.text = dialogue[index];
-            }
-            else
-            {
-                index++;
-                done = false;
-            }
-        }
-        if (!done)
-        {
-            if (textBox.text.Length == dialogue[index].Length)
-            {
-                done = true;
-            }
-            timer -= Time.deltaTime;
-            if (timer <= 0)
-            {
-                textBox.text += dialogue[index][textBox.text.Length];
-                timer = dialogue[index].Length / timePerBox;
-            }
-        }
-    }
+	
 }
