93ac61407cfddcdd05b84a3979b94a00b6d11667
nicohman <nicohman@demenses.net>
2019-05-14 16:09:48 -0700

    Pick title at random from list of randomly generated titles

---
+++ TeamNeoGame/Assets/Code/Components/TitlePick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TitlePick : MonoBehaviour
+{
+	private TextMeshProUGUI titleText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleText = GetComponent<TextMeshProUGUI>();
+        var allTitles = Resources.Load<TextAsset>("titles");
+        var listTitles = allTitles.text.Split('\n');
+        int index = Random.Range(0,listTitles.Length);
+        titleText.text = listTitles[index].ToUpper();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
