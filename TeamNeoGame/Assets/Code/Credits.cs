1c975bfbc0f5ed93d9ec0138aa6d5f0deab0514e
nicohman <nicohman@demenses.net>
2019-05-28 17:35:18 -0700

    Finish combat intro transition, fix bugs with combat resetting, add new assets & animations

---
+++ TeamNeoGame/Assets/Code/Credits.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Credits : MonoBehaviour
+{
+	private Rigidbody2D rigid;
+	public KeyCode skipkey;
+	public float timeToEnd = 40;
+	private float timer = 0;
+	public float waitEnd = 3;
+	private float waitTimer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    void End() {
+    	SceneManager.LoadScene("MainMenu");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    	waitTimer += Time.deltaTime;
+    	if (waitTimer >=waitEnd) {
+    		timer += Time.deltaTime;
+    		if (timer >= timeToEnd) {
+    			End();
+    		}
+    		if (Input.GetKeyDown(skipkey)) {
+    			End();
+    		}
+			rigid.velocity = new Vector2(0, 1);   
+		}     
+    }
+}
