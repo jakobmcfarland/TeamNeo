38855b345e84e23ed9138398dc15ed2165cff37e
William Siauw <36526721+Volcore001@users.noreply.github.com>
2019-05-30 17:05:41 -0700

    Updated Shop

--- TeamNeoGame/Assets/Code/Components/ShopMenuManager.cs
+++ TeamNeoGame/Assets/Code/Components/ShopMenuManager.cs
@@ -7,28 +7,41 @@ public class ShopMenuManager : MonoBehaviour
     public KeyCode enableKey;
     public GameObject menuObj;
     public GameObject player;
+    bool colliding = false;
     // Start is called before the first frame update
     void Start()
     {
         menuObj.SetActive(false);
     }
-
     // Update is called once per frame
-    void OnTriggerStay(Collider2D col)
+    void Update()
     {
-        Debug.Log("here");
-        if (Input.GetKeyDown(enableKey))
+        if (colliding)
         {
-            menuObj.SetActive(!menuObj.activeSelf);
-        }
+            if (Input.GetKeyDown(enableKey))
+            {
+                Debug.Log("herasde");
+                menuObj.SetActive(!menuObj.activeSelf);
+            }
 
-        if (menuObj.activeSelf || player.GetComponent<MapPlayerController>().storeMenu.activeSelf)
-        {
-            player.GetComponent<MapPlayerController>().enabled = false;
-            player.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
-            player.GetComponent<Animator>().Play("Idle");
+            if (menuObj.activeSelf || player.GetComponent<MapPlayerController>().storeMenu.activeSelf)
+            {
+                player.GetComponent<MapPlayerController>().enabled = false;
+                player.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+                player.GetComponent<Animator>().Play("Idle");
+            }
+            else
+                player.GetComponent<MapPlayerController>().enabled = true;
         }
-        else
-            player.GetComponent<MapPlayerController>().enabled = true;
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        colliding = true;
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        colliding = false;
+        
+
     }
 }
